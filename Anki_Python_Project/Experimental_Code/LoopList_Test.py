97ee3db006699c83984c2804c352377f6f2fc189
kondeeza <Myxoma@192.168.0.102>
2014-12-11 02:09:44 +1100

    more incomplete code

--- Anki_Python_Project/Experimental_Code/LoopList_Test.py
+++ Anki_Python_Project/Experimental_Code/LoopList_Test.py
@@ -22,12 +22,13 @@ for i in y:
 """
 
 
-input ="内乱^<br />反乱<br />乱す*^<br />乱れる*^<br />混乱<br />乱暴*^<br />"
+input ="内乱^<br />反乱<br />乱す*^<br />乱れる*^<br />反乱<br />乱暴*^<br />"
 
 y = input.split("<br />")
+"""
 x = []
 
-    
+
 for i in y:
     if (i.find("*") !=-1 or i.find("^") !=-1 ):
         x.append(i + "<br />")
@@ -40,3 +41,28 @@ for i in x:
     
 str1 = "".join(x)
 print str1
+"""
+
+
+def list_duplicates(seq):
+    seen = set()
+    seen_add = seen.add
+    # adds all elements it doesn't know yet to seen and all other to seen_twice
+    seen_twice = set( x for x in seq if x in seen or seen_add(x) )
+    # turn the set into a list (as requested)
+    return list( seen_twice )
+
+def listToString(input):
+    str1 = "".join(input)
+    return str1
+
+x =[]
+for i in y:
+    x.append(i + "<br />")
+
+x.pop()
+
+
+x = list_duplicates(x)
+x = listToString(x)
+print x
