b1e13502cf00f24d294ba9c1c8c614291410be84
kondeeza <kondeeza.123@gmail.com>
2019-03-06 09:56:22 +1100

    Initial Commit.

---
+++ 123222222/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# Entry point for the add-on into Anki
+# Please do not edit this if you do not know what you are doing.
+#
+# License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
+
+from . import Bulk_Generate_Hanguel_With_Similar_Homophone_List
