e5c41960b61357edc41f49d529d7d8736b203e85
kondeeza_SFpro2 <kondeeza.123@gmail.com>
2019-06-17 12:55:02 +0700

    more feature

--- Bulk_generate_Round_robin_list_html.py
+++ Bulk_generate_Round_robin_list_html.py
@@ -11,7 +11,14 @@ print ([x for x in itertools.chain.from_iterable(itertools.zip_longest(list1,lis
 
 #csvLocation = ""+os.path.dirname(__file__) + "\\Bulk_Generate_KrJp_def\\" + "KRDict_JP_All_Full_edited.csv"
 
-csvLocation = ""+os.path.dirname(__file__) + "\\list.csv"
+inputname = "list.csv"
+outputname = "The Second Coming_Dual Sub 101-200.html"
+csvLocation = ""+os.path.dirname(__file__) + "\\" + inputname
+
+"Kobo_Span"
+"Formatted_HTML"
+"Raw_Text"
+outputformat = "Formatted_HTML"
 def csvToRoundrobinList(fileName):
     L1 = []
     L2 = []
@@ -24,11 +31,21 @@ def csvToRoundrobinList(fileName):
 
 
     #now make file
-    with open('31.txt', 'w', encoding="utf-8") as f:
+    with open( outputname, 'w', encoding="utf-8") as f:
+
+        if outputformat == "Formatted_HTML":
+            f.write("""<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>""" + outputname + """</title><link href="HontoHtmlOutBasicCss.css" type="text/css" rel="stylesheet" /></head><body>""")
+
         for item in finalL:
-            f.write('<p class="calibre"><span xmlns="http://www.w3.org/1999/xhtml" class="kobospan"> %s </span></p>\n' % item)
+            if outputformat == "Kobo_Span":
+                f.write('<p class="calibre"><span class="kobospan"> %s </span></p>\n' % item)
+            elif outputformat == "Formatted_HTML":
+                f.write('<p class="calibre">%s </p>\n' % item)
+            elif outputformat == "Raw_Text":
+                f.write('%s\n' % item)
 
-
+        if outputformat == "Formatted_HTML":
+            f.write("< / body > < / html >")
 def makeRoundRobinList(L1,L2):
     return ([x for x in itertools.chain.from_iterable(itertools.zip_longest(L1,L2)) if x])
 
